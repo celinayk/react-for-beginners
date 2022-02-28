87173c8812ed039af7deba327bb75e4e41b98f90
Yeon Kyung Ryu <80758099+celinayk@users.noreply.github.com>
2022-02-28 16:51:03 +0900

    Update Movie.js

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -13,7 +13,7 @@ function Movie({id, coverImg, title, year, lan, backImg, summary, genres}) {
       <h3 className={styles.movie__year}>{year}</h3>
       <p>{summary.length > 235 ? `${summary.slice(0, 235)}...` : summary}</p>
       <ul className={styles.movie__genres}>
-        {genres.map((g) => ( //장르가 배열로 되어있기 때문에 이렇게 map써야함
+        {genres && genres.map((g) => ( //장르가 배열로 되어있기 때문에 이렇게 map써야함
           <li key={g}>{g}</li> //키값으로 g를 준 이유는 고유한 id가 없기때문에 g가 고유하다면 가능 
         ))}
       </ul>
